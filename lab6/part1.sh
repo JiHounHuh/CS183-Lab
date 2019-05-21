04754d0b5dee49985a243b752c7ae7c5e74ccdc3
PatrickWumbo <jhuh009@ucr.edu>
2019-05-21 14:26:55 -0700

    clean up

--- lab6/part1.sh
+++ lab6/part1.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 ps -Af | awk -F" " '$1 != "root" { print $1 ":" $2 ":" $7; }' > test.txt # get user processes
 cat test.txt | awk -F":" '{d1 = ( ($3 * 3600) + ($4 * 60) + $5); print $1 ":" $2 ":" d1}' > check.txt # convert time to seconds
-cat check.txt | awk -F":" '$3 > 120 {system("kill -9 $2) }' #kills procceses running longer than 2 min
+cat check.txt | awk -F":" '$3 > 120 {system("kill -9 " $2) }' #kills procceses running longer than 2 min
 # rm test.txt #clean up
 # rm check.txt #clean up
+echo " "
