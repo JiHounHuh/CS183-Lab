8ed9dcb61313ad592676b2af0b029272306c3bfa
root <root@localhost.localdomain>
2019-05-25 23:43:31 -0700

    new changes

--- Hw4/netmask.sh
+++ Hw4/netmask.sh
@@ -10,7 +10,21 @@ address=$(( 2 ** exp ))
 # echo $address
 usable=$(( address - 2 ))
 # netmask=$(( 255 - address + 1 ))
-check=$(ifconfig $Iface| awk '/netmask / { gsub("netmask:","",$4); print $4;}' | awk -F"." '{print $1 " " $2 " " $3 " " $4}')
-#newNet=$(( echo "obase=2;$Netmask" | bc))
-echo newNet
+check=$(ifconfig $Iface| awk '/netmask / { gsub("netmask:","",$4); print $4;}') 
+first=$(echo $check | awk -F"." '{print $1}')
+second=$(echo $check | awk -F"." '{print $2}')
+third=$(echo $check | awk -F"." '{print $3}')
+fourth=$(echo $check | awk -F"." '{print $4}')
+echo "first is $first"
+first=$( echo "obase=2;$first" | bc)
+second=$( echo "obase=2;$second" | bc)
+third=$(echo "obase=2;$third" | bc)
+fourth=$(echo "obase=2;$fourth" | bc)
+
+newCheck=$first"."$second"."$third"."$fourth
+echo $newCheck
+x=${Netmask##*255.}
+echo $x
+#echo $newCheck
+ 
 echo "addresses: $address usable: $usable netmask: 255.255.255.$netmask" 
