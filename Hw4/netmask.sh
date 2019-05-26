b696328d714a2b98932700d13d9a4dd1969c147f
PatrickWumbo <jhuh009@ucr.edu>
2019-05-25 23:03:16 -0700

    new implementation

--- Hw4/netmask.sh
+++ Hw4/netmask.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 param=$1
 base=32
+Iface=$(route -n | grep "UG" | awk '{print $8}')
+Netmask=$(ifconfig $Iface| awk '/netmask / { gsub("netmask:","",$4); print $4;exit}')
 exp=$(expr $base - $param)
-echo "this is the exponent $exp"
+# echo "this is the exponent $exp"
 address=$(( 2 ** exp ))
-echo $address
+# echo $address
 usable=$(( address - 2 ))
-netmask=$(( 255 - address + 1 ))
+# netmask=$(( 255 - address + 1 ))
+newNet=$(( echo "obase=2;$Netmask" | bc))
+echo newNet
 echo "addresses: $address usable: $usable netmask: 255.255.255.$netmask" 
